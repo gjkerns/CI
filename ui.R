69f10be3c156f99dc959a5b6125437b7bf085061
G. Jay Kerns <gkerns@ysu.edu>
2014-02-15 11:27:22 -0500

    small changes

--- ui.R
+++ ui.R
@@ -4,9 +4,7 @@ library(shiny)
 shinyUI(pageWithSidebar(
 
   # Application title.
-  headerPanel("Simulating Confidence Intervals: By Tyler Hunt"),
-  
-  
+  headerPanel("Simulating Confidence Intervals"),
   sidebarPanel(
     helpText("This app is a demonstration of confidence intervals.",
              "The length of the bars are the size of the intervals.",
@@ -16,11 +14,9 @@ shinyUI(pageWithSidebar(
     numericInput(inputId = "nsamp", label=strong("Sample Size"), value=100, min=5, max=10000),
     numericInput(inputId = "mean", label = strong("Mean"), value = 0, min=1, max=10000000),
     numericInput(inputId = "variance", label = strong("Variance"), value = 1, min=.01, max=100),
-    numericInput(inputId = "conf.level", label=strong("Confidence Level"), value=95, min=1, max=99)
+    numericInput(inputId = "conf.level", label=strong("Confidence Level"), value = 0.95, min=0.01, max=0.999999)
   ),
   
-  
-
   # Show a summary of the dataset and an HTML table with the requested
   # number of observations. Note the use of the h4 function to provide
   # an additional header above each output section.
